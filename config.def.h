5dfd912705d7015e86596011b84121582d7a2ac6
Ivan Lopes <lopesivan.ufrj@gmail.com>
2022-04-15 00:17:46 -0300

    Atualiza o arquivo config.def.h

--- config.def.h
+++ config.def.h
@@ -5,7 +5,9 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "FiraCode Nerd Font Mono:size=27:hinting=true:hintstyle=Regular:minspace=False";
+
+static char *font = "InconsolataLGC Nerd Font Mono:size=27:hinting=true:hintstyle=Regular:minspace=False";
+// static char *font = "FiraCode Nerd Font Mono:size=27:hinting=true:hintstyle=Regular:minspace=False";
 /* Spare fonts */
 static char *font2[] = {
     "Hack Nerd Font:pixelsize=27:antialias=true:autohint=true", // Powerline
