0c26d1a61815f00698c221b3749237ba450fbd7a
Ivan Lopes <lopesivan.ufrj@gmail.com>
2026-04-19 22:56:18 -0300

    :add: Adiciona o arquivo build.sh

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+n=$1
+
+font="$(fc-list : family | cut -f1 -d"," | sort | uniq | fzf)"
+
+[[ $font ]] || exit 2
+
+echo "=${font}="
+cp config.def.orig.h config.def.h
+
+font_size1=28
+font_size2=$((font_size1 + 2))
+sed "s/__FONT_NAME__:pixelsize=32/__FONT_NAME__:pixelsize=${font_size1}/" -i config.def.h
+sed "s/NotoColorEmoji:pixelsize=30/NotoColorEmoji:pixelsize=${font_size2}/" -i config.def.h
+
+sed "s/__FONT_NAME__/${font}/" -i config.def.h
+grep '^static char \*font =' config.def.h
+make clean
+make
+n=$(ls st.* | sort -V -r | head -1 | cut -d'.' -f 2)
+cp st st.$((++n))
+echo "$n : $font" | tee -a LOGS
+
+exit 0
