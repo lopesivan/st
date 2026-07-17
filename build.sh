c50781fac850f6ea16ea182d5f7f9323de169435
Ivan Lopes <lopesivan.ufrj@gmail.com>
2026-07-17 01:13:49 -0300

    :style: Atualiza o estilo do arquivo build.sh

--- build.sh
+++ build.sh
@@ -10,12 +10,11 @@ echo "=${font}="
 cp config.def.orig.h config.def.h
 
 font_size1=25
-font_size2=$((font_size1 + 2))
+font_size2=$((font_size1 - 2))
 sed "s/__FONT_NAME__:pixelsize=32/__FONT_NAME__:pixelsize=${font_size1}/" -i config.def.h
 sed "s/NotoColorEmoji:pixelsize=30/NotoColorEmoji:pixelsize=${font_size2}/" -i config.def.h
 
 sed "s/__FONT_NAME__/${font}/" -i config.def.h
-grep '^static char \*font =' config.def.h
 make clean
 make
 n=$(ls st.* | sort -V -r | head -1 | cut -d'.' -f 2)
